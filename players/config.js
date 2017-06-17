88534d2423461f2e8cb6a9a6adf77f781579197a
nischay30 <nischay.goyal@gmail.com>
2017-06-17 16:25:45 +0530

    players created with docker-compose

---
+++ players/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  expressPort: process.env.EXPRESS_PORT || 8081,
+  playerName: process.env.PLAYER_NAME || 'Nikhil',
+  defenceArrayLength: process.env.DEFENCE_ARRAY_LENGTH || 8
+}
